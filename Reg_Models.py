5bf88797d5c0b075a2d286f70877d5189afd1cc0
LORENZO SCARAMUSSA <lorenzoscaramussa@hotmail.com>
2021-08-23 17:08:20 -0300

    ifdjaisd

--- Reg_Models.py
+++ Reg_Models.py
@@ -4,21 +4,17 @@ from optuna.study import Study
 import xgboost
 import pandas as pd
 from catboost import CatBoostRegressor
-from sklearn.ensemble import AdaBoostRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn import neighbors
-from sklearn import linear_model
-from sklearn import ensemble
+from sklearn.ensemble import AdaBoostRegressor,RandomForestRegressor
+from sklearn import neighbors,linear_model,ensemble
 import lightgbm as lgbm
 from sklearn.metrics import mean_squared_error
 import numpy as np
 from source.source import calculate_metrics
+from sklearn.linear_model import RidgeCV, ElasticNet
 from tqdm import tqdm
-from sklearn.linear_model import RidgeCV
-
 
 class RegModels:
-    def __init__(self, modo, X_trainS, X_testS, y_trainS, y_testS, path="fitted_models") -> None:
+    def __init__(self, modo, X_trainS, X_testS, y_trainS, y_testS, path="models/ml_models") -> None:
         self.modo = modo
         self.path = path
         self.X_train = X_trainS
@@ -26,6 +22,7 @@ class RegModels:
         self.X_test = X_testS
         self.y_test = y_testS
         self.stack_model = RidgeCV()
+        
         global X_train
         X_train = self.X_train
         global y_train
@@ -65,7 +62,7 @@ class RegModels:
         else:
             return False
 
-    def fit_models(self, modelsList=False, path="fitted_models", verbose=True):
+    def fit_models(self, modelsList=False, path="models/ml_models", verbose=True):
         self.X_train = self.X_train
         self.y_train = self.y_train
         self.path = path
@@ -75,11 +72,12 @@ class RegModels:
 
         if not verbose:
             optuna.logging.set_verbosity(optuna.logging.WARNING)
-
+        
         if(self.modo == "optimize"):
             self.optimize_models(opt=True)
         else:
             self.optimize_models(opt=False)
+      
 
     def optimize_models(self, opt):
 
@@ -105,7 +103,7 @@ class RegModels:
             for m in modelsList:
                 qtd = qtd + modelsList[m]
             iteration = 0
-            for m in modelsList:
+            for m in tqdm(modelsList):
                 model = self.standard_models[m]
                 print('Optimizing model', m)
                 best_model, study = set_model(model, modelsList[m])
@@ -116,9 +114,7 @@ class RegModels:
                 print("\n\nModel concluded:  {} saved as '{}' \n ".format(
                     self.models_fit[k][1], m + ".sav"))
                 k += 1
-                # tqdm(iteration, total=qtd,
 
-                #      desc="Text You Want")
         else:
             import os
             savedModels = os.listdir(self.path)
@@ -129,7 +125,8 @@ class RegModels:
                     if((m + '.sav') == e):
                         self.models_fit.append(
                             (e, load_model(e, self.path).fit(X_train, y_train)))
-
+                        
+                        
     def visualize_slice(self, model_name, param_list=False):
         if self.study_list:
             fig = optuna.visualization.plot_slice(self.study_list[model_name])
@@ -137,9 +134,8 @@ class RegModels:
 
         else:
             print("No studies have been done by the class")
-
-    # faz o ensemble de todos os modelos de regressão para resultar em uma só previsao
-
+            
+    # Stack the models
     def stack_models(self, models_to_stack=False,  verbose=True):
         from sklearn.ensemble import StackingRegressor
 
@@ -228,7 +224,9 @@ class RegModels:
                     scoreList.append(model.score(X_test, y_test))
                     test_metrics.append(calculate_metrics(y_test, test_pred, m))
                 else:
-                     if(m in modelsList): 
+                   
+                    if(m[:-4] in modelsList): 
+                        model = load_model(m, self.path)
                         test_pred = model.predict(X_test)
                         scoreList.append(model.score(X_test, y_test))
                         test_metrics.append(calculate_metrics(y_test, test_pred, m))
@@ -247,16 +245,15 @@ class RegModels:
         self.stack_model = final
         return final
 
-
+########################################
+    
 def set_model(model, n_trials):
     study = optuna.create_study(direction="maximize")
-
     study.optimize(model, n_trials=n_trials, callbacks=[callback])
-
     print(study.best_trial)
     best_model = study.user_attrs["best_model"]
 
-    return best_model, study
+    return best_model,study
 
 
 def save_model(model, name, path):
@@ -290,168 +287,193 @@ def score_method(model, trial):
         return intermediate_value
 
 
-# funcoes com os modelos de regressao a serem otimizados.
-# Mais modelos podem ser adicionados, só é necessário que o retorno e a estrutura geral seja a mesma
-# Esses modelos ja estao com algumas sugestoes de otimização, contudo mais opções podem ser adicionadas
-
-
-def obj_ridge_CV(trial):
-    # h_alpha = trial.suggest_float("n_alphas", 0.1, 1)
-    h_random_state = trial.suggest_int("random_state", 1, 50)
-    cv = trial.suggest_categorical("cv", [None,  2, 3, 4, 5])
-
-    if cv == None:
-        alpha_per_target = trial.suggest_categorical(
-            "alpha_per_target", [True, False])
-        model = RidgeCV(
-            cv=cv, alpha_per_target=alpha_per_target)
-    else:
-        alpha_per_target = trial.suggest_categorical(
-            "alpha_per_target", [True, False])
-        model = RidgeCV(
-            cv=cv)
-
-    trial.set_user_attr(key="best_model", value=model)
-
-    return score_method(model, trial)
-
+###################################
+#             Models
+###################################
 
 def obj_lasso(trial):
-    h_alpha = trial.suggest_float("alpha", 0.1, 1.0)
-
+    h_alpha = trial.suggest_float("alpha", 0.02, 1.0, log=True)
     model = linear_model.Lasso(alpha=h_alpha)
     trial.set_user_attr(key="best_model", value=model)
 
     return score_method(model, trial)
-# min_samples_split=h_min_samples_split,
-# max_depth=h_max_depth, n_estimators=h_n_estimators, min_samples_leaf=h_min_samples_leaf
-
-
-def obj_random_forest(trial):
-    f_n_estimators = trial.suggest_int("n_estimators", 100, 500)  # descrição
-    f_max_depth = trial.suggest_int("max_depth",10,50)
-    f_min_samples_leaf = trial.suggest_categorical("leaf", [1, 2, 4])
-    f_min_samples_split = trial.suggest_categorical(
-        "samples_split", [2, 5, 10])
-    int_max_features = np.linspace(len(X_train.columns),10,5,dtype=int) 
-    
-    max_features_list = ["auto","sqrt","log2",None] 
-
-
-    for e in int_max_features: 
-        max_features_list.append(int(e))
-
-
-    f_max_features = trial.suggest_categorical("max_features",max_features_list )
-    model = ensemble.RandomForestRegressor(max_depth =f_max_depth, n_estimators = f_n_estimators, min_samples_split = f_min_samples_split, min_samples_leaf = f_min_samples_leaf, max_features = f_max_features, n_jobs = -1)
-    trial.set_user_attr(key="best_model", value=model)
-
-    return score_method(model, trial)
-
-
-def obj_elastic_net(trial):
-    e_alpha = trial.suggest_float("alpha",0.1,1)
-    # e_random_state = trial.suggest_int("random_state", 1, 50)
-    # e_max_iter = trial.suggest_int("max_iter",100,10000)
-    e_l1_ratio = trial.suggest_float("l1_ratio",0.1,1)
-    # e_cv = trial.suggest_int("cv", 3, 5)
-    model = linear_model.ElasticNet(
-       alpha=e_alpha,l1_ratio = e_l1_ratio )
-    trial.set_user_attr(key="best_model", value=model)
-
-    return score_method(model, trial)
-
-
-def obj_ada_boost(trial):
-    h_n_estimators = trial.suggest_int("n_estimators", 10, 1000)  # descrição
-    h_random_state = trial.suggest_int("random_state", 1, 50)
-    h_learning_rate = trial.suggest_float("learning_rate", 0.01, 1)
-    h_loss = trial.suggest_categorical(
-        "loss", ['linear', 'square', 'exponential'])
-    model = ensemble.AdaBoostRegressor(random_state=h_random_state, learning_rate=h_learning_rate, loss=h_loss,
-                                       n_estimators=h_n_estimators)
-    trial.set_user_attr(key="best_model", value=model)
-
-    return score_method(model, trial)
-
 
 def obj_ridge(trial):
-    h_alpha = trial.suggest_float("alpha",0.1, 500)
+    h_alpha = trial.suggest_float("alpha", 0.01, 5.0, log=True)
     model = linear_model.Ridge(alpha=h_alpha)
     trial.set_user_attr(key="best_model", value=model)
 
     return score_method(model, trial)
 
 
-def obj_XGBRegressor(trial):
-    x_max_deph = trial.suggest_int("max_depth",3,10)
-    x_learning_rate = trial.suggest_float("learning_rate",0.01,0.1)
-    x_n_estimators = trial.suggest_int("n_estimators",100,1000)
-    x_colsample_bytree = trial.suggest_float("bytree",0.1,0.9)
-    x_min_child_weight =  trial.suggest_int("min_child_weight",4,9)
-    model = xgboost.XGBRegressor(max_deph = x_max_deph,learning_rate = x_learning_rate, n_estimators = x_n_estimators, colsample_bytree = x_colsample_bytree, min_child_weight  =x_min_child_weight   )
+def obj_elastic_net(trial):
+    e_alpha = trial.suggest_int("n_alphas", 1, 100, log=True)
+    e_l1_ratio = trial.suggest_float("l1_ratio",0.1,1)
+    model = linear_model.ElasticNet(alpha=e_alpha,l1_ratio = e_l1_ratio)
+    
+    trial.set_user_attr(key="best_model", value=model)
+
+    return score_method(model, trial)
+
+
+def obj_random_forest(trial):
+    f_n_estimators = trial.suggest_int("n_estimators", low=20,high=200,step=10)
+    f_max_depth = trial.suggest_int("max_depth", low=2,high=30,step=2)
+    f_min_samples_leaf = trial.suggest_int("min_samples_leaf", low=1, high=8, step=1)
+    f_min_samples_split = trial.suggest_int("min_samples_split", low=2, high=8, step=1)
+    
+    #int_max_features = np.arange(24,len(X_train.columns),1,dtype=int) 
+    #max_features_list = []
+    #for e in int_max_features: 
+    #    max_features_list.append(int(e))
+    #f_max_features = trial.suggest_categorical("max_features",max_features_list) 
+     
+    model = ensemble.RandomForestRegressor(max_depth=f_max_depth,
+                                           n_estimators = f_n_estimators,
+                                           min_samples_split = f_min_samples_split,
+                                           min_samples_leaf = f_min_samples_leaf,
+                                           #max_features = f_max_features,
+                                           random_state=0, 
+                                           n_jobs = -1)
+
     trial.set_user_attr(key="best_model", value=model)
 
     return score_method(model, trial)
 
 
 def obj_extra_trees(trial):
-    e_n_estimators = trial.suggest_int("n_estimators", 10, 100)  # descrição
-    e_random_state = trial.suggest_int("random_state", 1, 50)
-    e_min_samples_split = trial.suggest_int("min_samples_split", 2, 5)
-    e_min_samples_leaf = trial.suggest_int("min_samples_leaf", 1, 5)
-    e_oob_score = trial.suggest_categorical("oob_score",[True,False])
-    e_bootstrap = e_oob_score if False else True # necessary for the model to work 
+    e_n_estimators = trial.suggest_int("n_estimators", low=20, high=200, step=10)
+    e_min_samples_split = trial.suggest_int("min_samples_split", low=2, high=8, step=1)
+    e_min_samples_leaf = trial.suggest_int("min_samples_leaf", low=1, high=8, step=1)
+    e_oob_score = trial.suggest_categorical("bootstrap",[True,False])
+    e_bootstrap = e_oob_score if False else True # necessary for the model to work
     
-    model = ensemble.ExtraTreesRegressor(oob_score = e_oob_score, min_samples_leaf=e_min_samples_leaf,
-                                         min_samples_split=e_min_samples_split, n_estimators=e_n_estimators, random_state=e_random_state, bootstrap = e_bootstrap)
-    trial.set_user_attr(key="best_model", value=model )
+    #int_max_features = np.arange(24,len(X_train.columns),1,dtype=int) 
+    #max_features_list = []
+    #for e in int_max_features: 
+    #    max_features_list.append(int(e))
+    #f_max_features = trial.suggest_categorical("max_features",max_features_list) 
+    
+    model = ensemble.ExtraTreesRegressor(oob_score = e_oob_score,
+                                         min_samples_leaf=e_min_samples_leaf,
+                                         min_samples_split=e_min_samples_split,
+                                         n_estimators=e_n_estimators,
+                                         bootstrap = e_bootstrap,
+                                         #max_features = f_max_features,
+                                         random_state=0, 
+                                         n_jobs=-1)
+
+    trial.set_user_attr(key="best_model", value=model)
 
     return score_method(model, trial)
 
 
+def obj_ada_boost(trial):
+    h_n_estimators = trial.suggest_int("n_estimators", low=20, high=500, step=10)
+    h_learning_rate = trial.suggest_float("learning_rate", 0.01, 1)
+    h_loss = trial.suggest_categorical("loss", ['linear', 'square', 'exponential'])
+    
+    model = ensemble.AdaBoostRegressor(learning_rate=h_learning_rate,
+                                       loss=h_loss,
+                                       n_estimators=h_n_estimators,
+                                       random_state=0)
+    
+    trial.set_user_attr(key="best_model", value=model)
+
+    return score_method(model, trial)
+
+
+
+def obj_XGBRegressor(trial):
+    x_max_depth = trial.suggest_int("max_depth",2,20)
+    x_learning_rate = trial.suggest_float("learning_rate",0.01,0.1)
+    x_n_estimators = trial.suggest_int("n_estimators", low=50, high=600, step=10)
+    x_colsample_bytree = trial.suggest_float("bytree",0.1,0.9)
+    x_min_child_weight =  trial.suggest_int("min_child_weight",4,12)
+    
+    model = xgboost.XGBRegressor(max_depth = x_max_depth,
+                                 learning_rate = x_learning_rate,
+                                 n_estimators = x_n_estimators,
+                                 colsample_bytree = x_colsample_bytree,
+                                 min_child_weight  =x_min_child_weight,
+                                 seed=123,
+                                 gpu_id=0,
+                                 tree_method='gpu_hist',
+                                 random_state=0,
+                                 n_jobs=-1)
+    
+    trial.set_user_attr(key="best_model", value=model)
+
+    return score_method(model, trial)
+
+
+
 def obj_KNeighborsRegressor(trial):
-    h_n_neighbors = trial.suggest_int("n_neighbors", 2, 7)
-    model = neighbors.KNeighborsRegressor(n_neighbors=h_n_neighbors)
+    h_n_neighbors = trial.suggest_int("n_neighbors", 1, 8)
+    h_weights = trial.suggest_categorical("weights", ['uniform','distance'])
+    distance_metric =trial.suggest_categorical("distance_metric", ['euclidean','manhattan'])
+    
+    model = neighbors.KNeighborsRegressor(n_neighbors=h_n_neighbors,weights=h_weights,metric=distance_metric)
+    
     trial.set_user_attr(key="best_model", value=model)
 
     return score_method(model, trial)
 
 
 def obj_SGDRegressor(trial):
-    sdg_loss = trial.suggest_categorical(
-        "sdg_loss", ['squared_loss', 'huber', 'epsilon_insensitive'])
-    sdg_penalty = trial.suggest_categorical(
-        "penalty", ['l1', 'l2', 'elasticnet'])
-    sdg_alpha = trial.suggest_float('alpha',0.0001,1)
-    model = linear_model.SGDRegressor(
-        verbose=0, penalty=sdg_penalty, loss=sdg_loss, alpha=sdg_alpha)
+    sdg_loss = trial.suggest_categorical("sdg_loss", ['squared_loss', 'huber', 'epsilon_insensitive'])
+    sdg_penalty = trial.suggest_categorical("penalty", ['l1', 'l2', 'elasticnet'])
+    sdg_alpha = trial.suggest_float('alpha',0.001,1)
+    
+    model = linear_model.SGDRegressor(penalty=sdg_penalty, loss=sdg_loss,alpha=sdg_alpha,verbose=0)
+    
     trial.set_user_attr(key="best_model", value=model)
-
+    
     return score_method(model, trial)
 
 
 def obj_catBoostRegressor(trial):
-    cat_n_estimators = trial.suggest_int("n_estimators", 800, 2000)
+    cat_n_estimators = trial.suggest_int("n_estimators", low=20, high=500, step=10)
+    cat_loss = trial.suggest_categorical("MAE", ['squared_loss', 'MAPE', 'Poisson'])
     cat_depth = trial.suggest_int("depth",8,12)
-    model = CatBoostRegressor(
-         n_estimators=cat_n_estimators,
-        depth=10,
-        verbose=False
-    )
+    
+    model = CatBoostRegressor(n_estimators=cat_n_estimators,
+                              depth=cat_depth,
+                              loss_function=cat_loss,
+                              verbose=False)
+    
     trial.set_user_attr(key="best_model", value=model)
 
     return score_method(model, trial)
 
 
 def obj_LightBoost(trial):
-    light_min_samples_split = trial.suggest_int("min_samples_split", 200, 1001)
-    light_max_depth = trial.suggest_int("max_depth", 7, 14)
-    model = lgbm.LGBMRegressor(max_depth=light_max_depth,
-                               min_samples_split=light_min_samples_split, subsample=0.8, random_state=10)
+    light_max_depth = trial.suggest_int("max_depth", low=4,high=100,step=2)
+    light_num_leaves = trial.suggest_int("num_leaves", low=50,high=300,step=5)
+    light_estimators = trial.suggest_int("num_estimators", low=100,high=800,step=10)
+
+    model = lgbm.LGBMRegressor(objective='regression',
+                               max_depth=light_max_depth,
+                               num_leaves = light_num_leaves,
+                               n_estimators = light_estimators,
+                               device='gpu',
+                               random_state=0,
+                               silent=True,
+                               n_jobs=-1)
+                              
     trial.set_user_attr(key="best_model", value=model)
 
     return score_method(model, trial)
+
+def obj_ridge_CV(trial):
+    h_alpha = trial.suggest_float("n_alphas", 0.01, 5)
+    alpha_per_target = trial.suggest_categorical("alpha_per_target", [True, False])
+    model = RidgeCV(alphas=h_alpha,alpha_per_target=alpha_per_target)
+
+    trial.set_user_attr(key="best_model", value=model)
+
+    return score_method(model, trial)
+
 
 
 def callback(study, trial):
